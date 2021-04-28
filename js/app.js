5c7bc450f814d16263a40f93b43aab0ee7c98c90
majdmasadeh <majdmasadeh1995@gmail.com>
2021-04-28 19:13:27 +0300

    lab04-functions

--- js/app.js
+++ js/app.js
@@ -2,20 +2,21 @@
 
 'use strict';
 let score = 0;
-var username = prompt("Please Enter your name").toUpperCase();
+var username = prompt("Please Enter your name").toUpperCase()
 alert ("Welcome"+username);
 alert('let\'s play a guessing game about me.');
 
 function quizSuring (){
     let sureQuestion = prompt('are you ready to have this quiz? if yes insert Y else insert N ').toUpperCase()
-    //console.log(sureQuestion)
+    
     switch (sureQuestion) {
         case 'Y':
-            alert('pres ok to continue')
+            alert('pres ok to continue');
+            score=score+1;
             break;
         case 'N':
-            alert('close the Tab to leave this site')
-            break
+            alert('close the Tab to leave this site');
+            break;
         default:
             text = 'this is not an expected value try again'
             console.log(text);
@@ -30,7 +31,8 @@ function MYMAJOR(){
 console.log(mymajor);
 if (mymajor==='yes' || mymajor==='y')
 {alert(' Wrong ans.');}
-else{alert('correct Ans.');}
+else{alert('correct Ans.');
+score=score+1}
 
 }
 MYMAJOR();
@@ -40,7 +42,8 @@ function sport(){
 let sportQues=prompt('is majd like sport?(yes/y or no/n)');
 console.log(sportQues);
 if (sportQues==='yes' || sportQues==='y')
-{alert('correct ans.');}
+{alert('correct ans.');
+score=score+1;}
 else{alert('Wrong Ans.');}
 }
 sport();
@@ -90,7 +93,8 @@ return animals;
     let myhome = prompt('do I live in Irbid ? yes or no');
     console.log(myhome)
     if (myhome==='yes' || myhome ==='y')
-{alert(' correct ans.');}
+{alert(' correct ans.');
+score=score+1;}
 else{alert(' Wrong Ans.');}
 
     }
@@ -101,7 +105,8 @@ function place(){
 let favPlace=prompt('is majds  favourite place Lebanon?(yes/y or no/n)');
 console.log(favPlace);
 if (favPlace==='yes' || favPlace==='y')
-{alert('correct ans.');}
+{alert('correct ans.');
+score=score+1;}
 else{alert('Wrong Ans.');}
 }
 
@@ -111,65 +116,26 @@ function MYDRINK(){
     let mydrink = prompt('Is American coffee my Favourite drink ? yes or no');
     console.log(mydrink);
     if (mydrink==='yes' || mydrink==='y')
-{alert('correct ans.');}
+{alert('correct ans.');
+score=score+1;}
 else{alert('Wrong Ans.');
     
 }
-} 
+}
+    
     MYDRINK();
+   
     
 
-/*let majd=prompt('Enter your name !!');
-console.log(majd);
-
-let age=prompt('are your above 18? (y/n)');
-
-if(age ==='y')
-{
-  console.log('you are welcome');
-}else{console.log('go to kids part');
-}
-
-let question1=prompt('Is Majd an Engineer ? (yes/y or no/n)').toLowerCase();
-console.log(question1);
-if (question1==='yes' || question1==='y')
-{alert('Correct Ans.');}
-else{alert('Wrong Ans.');}
-
-let question2=prompt('is majd like sport?(yes/y or no/n)');
-console.log(question2);
-if (question2==='yes' || question2==='y')
-{alert('correct ans.');}
-else{alert('Wrong Ans.');}
-
-
-let question3=prompt('is majds  like turtiles?(yes/y or no/n)');
-console.log(question3);
-if (question3==='yes' || question3==='y')
-{alert('correct ans.');}
-else{alert('Wrong Ans.');}
-
-let question4=prompt('is majds  favourite color black?(yes/y or no/n)');
-console.log(question4);
-if (question4==='yes' || question4==='y')
-{alert('Wrong Ans.');}
-else{alert('correct ans');}
-
-let question5=prompt('is majds  favourite place Lebanon?(yes/y or no/n)');
-console.log(question5);
-if (question5==='yes' || question5==='y')
-{alert('correct ans.');}
-else{alert('Wrong Ans.');}
-
-
-
-
-
-
-// let work = prompt('do you work? (y/n)');
-// console.log('do you work?'+work);
-// let sport = prompt('do you making sport daily?');
-// console.log('do you making sport daily' +sport);
-
-
-*/
+    document.write('<br>');
+    document.write('<p> I am an Engineer </p>');
+    document.write('<br>');
+    document.write('<p> my fav animals are: ' + animals + '</p>');
+    document.write("<pre>");
+    alert('Your Score is ' + score + '/7 ');
+    document.write('<br>');
+    document.write('<p> you have guessed ' + score + 'question about me </p>');
+    document.write('<br>');
+    document.write('<br>');
+    alert('welcome ' + username + ' and thanks for doing the quiz,I wish to you the best ');
+    document.write('<p> ^_^ thanks for doing the quiz, I wish to you the best ^_^ </p> ');
