fa409056239bf8d1d6fc1940694c2127434dec05
majdmasadeh <majdmasadeh1995@gmail.com>
2021-04-28 18:35:10 +0300

    lab03mod

--- js/app.js
+++ js/app.js
@@ -1,5 +1,125 @@
 'use strict';
-let majd=prompt('Enter your name !!');
+
+'use strict';
+let score = 0;
+var username = prompt("Please Enter your name").toUpperCase();
+alert ("Welcome"+username);
+alert('let\'s play a guessing game about me.');
+
+function quizSuring (){
+    let sureQuestion = prompt('are you ready to have this quiz? if yes insert Y else insert N ').toUpperCase()
+    //console.log(sureQuestion)
+    switch (sureQuestion) {
+        case 'Y':
+            alert('pres ok to continue')
+            break;
+        case 'N':
+            alert('close the Tab to leave this site')
+            break
+        default:
+            text = 'this is not an expected value try again'
+            console.log(text);
+            break;
+    }
+    }
+    quizSuring ();
+    
+
+function MYMAJOR(){
+    let mymajor = prompt('Iam a Singer ? yes or no'); 
+console.log(mymajor);
+if (mymajor==='yes' || mymajor==='y')
+{alert(' Wrong ans.');}
+else{alert('correct Ans.');}
+
+}
+MYMAJOR();
+
+
+function sport(){
+let sportQues=prompt('is majd like sport?(yes/y or no/n)');
+console.log(sportQues);
+if (sportQues==='yes' || sportQues==='y')
+{alert('correct ans.');}
+else{alert('Wrong Ans.');}
+}
+sport();
+
+
+let animals = ['turtile', 'cat', 'dog'];
+function fAnimals(){
+let userAns = prompt('Guess What is my fav animal ? ');
+
+let flag = false;
+for (let t = 1; t < 3; t++) {
+    for (let f = 0; f < animals.length; f++) {
+        if (userAns == animals[f]) {
+            alert('You got it  ');
+           
+
+            flag = true;
+            score++;
+            return score;
+            break;
+        }
+
+
+    }
+    if (flag == true) { break; }
+    else {
+        alert(userAns + " Is a Wrong Answer Please try again ");
+        userAns = prompt('Try Again');
+
+    }
+}
+
+
+if (flag == false) {
+    alert('Try harder Next Time');
+    alert('The right answers is : ' + animals);
+    document.write('<br>');
+    document.write('my fav animal is answers is : ' + animals);
+
+}
+return animals;
+}
+ fAnimals();
+
+
+ function MYHOME (){
+    let myhome = prompt('do I live in Irbid ? yes or no');
+    console.log(myhome)
+    if (myhome==='yes' || myhome ==='y')
+{alert(' correct ans.');}
+else{alert(' Wrong Ans.');}
+
+    }
+    
+    MYHOME();
+
+function place(){
+let favPlace=prompt('is majds  favourite place Lebanon?(yes/y or no/n)');
+console.log(favPlace);
+if (favPlace==='yes' || favPlace==='y')
+{alert('correct ans.');}
+else{alert('Wrong Ans.');}
+}
+
+place();
+
+function MYDRINK(){
+    let mydrink = prompt('Is American coffee my Favourite drink ? yes or no');
+    console.log(mydrink);
+    if (mydrink==='yes' || mydrink==='y')
+{alert('correct ans.');}
+else{alert('Wrong Ans.');
+    
+}
+} 
+    MYDRINK();
+    
+
+/*let majd=prompt('Enter your name !!');
 console.log(majd);
 
 let age=prompt('are your above 18? (y/n)');
@@ -40,7 +160,7 @@ console.log(question5);
 if (question5==='yes' || question5==='y')
 {alert('correct ans.');}
 else{alert('Wrong Ans.');}
-/* jjjjj*/
+
 
 
 
@@ -52,3 +172,4 @@ else{alert('Wrong Ans.');}
 // console.log('do you making sport daily' +sport);
 
 
+*/
