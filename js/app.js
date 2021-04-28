5c8f24258d0999c43b9708d46a5042d4362d5e34
majdmasadeh <majdmasadeh1995@gmail.com>
2021-04-28 15:44:23 +0300

    lab03-majd

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+let majd=prompt('Enter your name !!');
+console.log(majd);
+
+let age=prompt('are your above 18? (y/n)');
+
+if(age ==='y')
+{
+  console.log('you are welcome');
+}else{console.log('go to kids part');
+}
+
+let question1=prompt('Is Majd an Engineer ? (yes/y or no/n)').toLowerCase();
+console.log(question1);
+if (question1==='yes' || question1==='y')
+{alert('Correct Ans.');}
+else{alert('Wrong Ans.');}
+
+let question2=prompt('is majd like sport?(yes/y or no/n)');
+console.log(question2);
+if (question2==='yes' || question2==='y')
+{alert('correct ans.');}
+else{alert('Wrong Ans.');}
+
+
+let question3=prompt('is majds  like turtiles?(yes/y or no/n)');
+console.log(question3);
+if (question3==='yes' || question3==='y')
+{alert('correct ans.');}
+else{alert('Wrong Ans.');}
+
+let question4=prompt('is majds  favourite color black?(yes/y or no/n)');
+console.log(question4);
+if (question4==='yes' || question4==='y')
+{alert('Wrong Ans.');}
+else{alert('correct ans');}
+
+let question5=prompt('is majds  favourite place Lebanon?(yes/y or no/n)');
+console.log(question5);
+if (question5==='yes' || question5==='y')
+{alert('correct ans.');}
+else{alert('Wrong Ans.');}
+
+
+
+
+
+// let work = prompt('do you work? (y/n)');
+// console.log('do you work?'+work);
+// let sport = prompt('do you making sport daily?');
+// console.log('do you making sport daily' +sport);
+
+
